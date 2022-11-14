03afce1ca9fdc8043cabe22c56845146340245e0
AdrianKnapp <adrknapp@gmail.com>
2022-11-14 11:54:51 -0300

    Added font family

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,6 @@
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
 import styles from './styles/app.css';
+import global from './styles/global.css';
 import type { MetaFunction } from '@remix-run/node';
 
 export const meta: MetaFunction = () => ({
@@ -9,7 +10,25 @@ export const meta: MetaFunction = () => ({
 });
 
 export function links() {
-  return [{ rel: 'stylesheet', href: styles }];
+  return [
+    { rel: 'stylesheet', href: styles },
+    {
+      rel: 'stylesheet',
+      href: global,
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com',
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.gstatic.com',
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap',
+    },
+  ];
 }
 
 const App = () => {
