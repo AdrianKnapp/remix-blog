cfbd7584d8b309cb7f5a8808675889a7b428b7e1
AdrianKnapp <adrknapp@gmail.com>
2022-11-15 08:59:28 -0300

    Added post page

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,5 +1,31 @@
-const Post = () => {
-  return <h1>Post page</h1>;
+import { marked } from 'marked';
+import { json } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import invariant from 'tiny-invariant';
+import { getPost } from '~/models/post.server';
+import type { Post } from '~/models/post.server';
+import type { LoaderFunction } from '@remix-run/node';
+
+type LoaderData = { post: Post; html: string };
+
+export const loader: LoaderFunction = async ({ params }) => {
+  invariant(params.slug, `params.slug is required`);
+
+  const post = await getPost(params.slug);
+  invariant(post, `Post not found: ${params.slug}`);
+
+  const html = marked(post.markdown);
+  return json<LoaderData>({ post, html });
+};
+const PostSlug = () => {
+  const { post, html } = useLoaderData();
+
+  return (
+    <main className="mx-auto max-w-4xl">
+      <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
+      <div className="mt-2" dangerouslySetInnerHTML={{ __html: html }} />
+    </main>
+  );
 };
 
-export default Post;
+export default PostSlug;
